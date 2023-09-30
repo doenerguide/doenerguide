900e1e15341ce845a78928989471c50fd7e675ef
Kagan Demirer <demirerkagan0808@gmail.com>
2023-09-30 16:51:30 +0200

    Neuer Test

--- backend/deploy.py
+++ backend/deploy.py
@@ -1,4 +1,4 @@
-# a script, that pulls the latest version of the backend from github
+# a script, that pulls the latest version of the backend from github and logs the output
 
 import os
 import subprocess
@@ -7,4 +7,12 @@ import subprocess
 os.chdir("/mnt/web310/b1/02/512442602/htdocs/doenerguide")
 
 # pull the latest version from github
-subprocess.call(["git", "pull"])
+process = subprocess.Popen(["git", "pull"], stdout=subprocess.PIPE)
+output = process.communicate()[0]
+
+# write the output to the log file
+with open("log.txt", "a") as logfile:
+    logfile.write(output)
+
+# print the output to the console
+print(output)
