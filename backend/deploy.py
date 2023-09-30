cb55c2a23b9ea4ed035d49b6312fff7f30d229b7
Kagan Demirer <demirerkagan0808@gmail.com>
2023-09-30 17:08:37 +0200

    Deploy.py fertig

--- backend/deploy.py
+++ backend/deploy.py
@@ -1,18 +1,13 @@
-# a script, that pulls the latest version of the backend from github and logs the output
-
-import os
 import subprocess
 
-# change to the /mnt/web310/b1/02/512442602/htdocs/doenerguide directory
-os.chdir("/mnt/web310/b1/02/512442602/htdocs/doenerguide")
+def deploy():
+    # Construct the command as a single string
+    command = 'ssh xn--dnerguide-07a.de@ssh.strato.de "cd /mnt/web310/b1/02/512442602/htdocs/doenerguide && git pull"'
 
-# pull the latest version from github
-process = subprocess.Popen(["git", "pull"], stdout=subprocess.PIPE)
-output = process.communicate()[0]
+    # Run the command using subprocess.run()
+    process = subprocess.run(command, shell=True, stdout=subprocess.PIPE, text=True)
 
-# write the output to the log file
-with open("log.txt", "a") as logfile:
-    logfile.write(output)
+    # Print the output
+    print(process.stdout)
 
-# print the output to the console
-print(output)
+deploy()
