2c6cb8709c7c9596c71d4fcc8abb4d10d9cc9f75
Kagan Demirer <demirerkagan0808@gmail.com>
2023-10-04 09:54:18 +0200

    Test

--- backend/deploy.py
+++ backend/deploy.py
@@ -1,13 +1,3 @@
 import subprocess
-
-def deploy():
-    # Construct the command as a single string
-    command = 'ssh xn--dnerguide-07a.de@ssh.strato.de "cd /mnt/web310/b1/02/512442602/htdocs/doenerguide && git pull"'
-
-    # Run the command using subprocess.run()
-    process = subprocess.run(command, shell=True, stdout=subprocess.PIPE, text=True)
-
-    # Print the output
-    print(process.stdout)
-
-deploy()
+command = 'ssh xn--dnerguide-07a.de@ssh.strato.de "cd /mnt/web310/b1/02/512442602/htdocs/doenerguide && git pull"'
+process = subprocess.run(command, shell=True, stdout=subprocess.PIPE, text=True)
