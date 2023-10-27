52a9c0211ac2e832359f35873aa53dcd3ed7eb3f
johnmax23 <johnmax23>
2023-10-27 20:25:44 +0200

    Hinzufügen einer Permission Spalte
    1. Funktion (clear_database) für Debugging, löscht die User Tabelle
    
    2. Funktion (create_table) erstellt Tabelle [jetzt als Funktion]
        - "permissions"- [Berechtigungs-] Spalte hinzugefügt
    
    3. Funktion (add_user)
        - hinzufügen des "permission" Parameters
    
    4. Funktion (get_permissions) gibt die Berechtigungen eines Users zurück
    
    5. Funktion (convert_to_json) konvertiert die Tabelle in JSON Format
        - nur für Debugging
    
    6. Sonstiges
        - hinzufügen und Überprüfen von Usern als Test mit den Funktionen

--- backend/login.py
+++ backend/login.py
@@ -1,13 +1,27 @@
 import sqlite3
 import json
 
-# Verbindung zur SQL Database
-conn = sqlite3.connect('users.db')
+DB_NAME = 'users.db'
 
-# Erstellt SQL Tabelle
-conn.execute('''CREATE TABLE IF NOT EXISTS users
+# Verbindung zur SQL Database
+conn = sqlite3.connect(DB_NAME)
+
+###############################################
+# NUR FUER DEBUGGING:
+# Löscht die database
+def clear_database():
+    conn.execute("DROP TABLE IF EXISTS users")
+    conn.commit()
+    
+clear_database()
+###############################################
+    
+# Erstellt SQL Tabelle mit Benutzername, Passwort und Berechtigungen
+def create_table():
+    conn.execute('''CREATE TABLE IF NOT EXISTS users
              (username TEXT PRIMARY KEY NOT NULL,
-             password TEXT NOT NULL);''')
+             password TEXT NOT NULL,
+             permissions TEXT NOT NULL);''')
 
 # Überprüfung, ob User existiert
 def user_exists(username):
@@ -19,10 +33,10 @@ def user_exists(username):
         return True
 
 # Hinzufügen neuer User
-def add_user(username, password):
-    conn.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, password))
+def add_user(username, password, permission):
+    conn.execute("INSERT INTO users (username, password, permissions) VALUES (?, ?, ?)", (username, password, permission))
     conn.commit()
-    
+
 # Entfernen von Usern
 def remove_user(username):
     conn.execute("DELETE FROM users WHERE username=?", (username,))
@@ -39,3 +53,45 @@ def authenticate(username, password):
             return True
         else:
             return False
+
+# Überprüft Berechtigungen der User
+def get_permissions(username):
+    cursor = conn.execute("SELECT permissions FROM users WHERE username=?", (username,))
+    row = cursor.fetchone()
+    if row is None:
+        return None
+    else:
+        return row[0]
+
+create_table()
+test_user="Max"
+add_user(test_user,"pw","user")
+###############################
+# NUR FUER DEBUGGING:
+add_user("admin","123","admin")
+###############################
+
+if user_exists(test_user):
+    print("True")
+    print(get_permissions(test_user))
+else:
+    print("False")
+
+####################################################################
+# NUR FUER DEBUGGING:
+# Konvertiert .db zu .json um einen besseren Ueberblick zu behalten
+def convert_to_json():
+            cursor = conn.execute("SELECT * FROM users")
+            rows = cursor.fetchall()
+            users = []
+            for row in rows:
+                user = {}
+                user['username'] = row[0]
+                user['password'] = row[1]
+                user['permissions'] = row[2]
+                users.append(user)
+            with open('users.json', 'w') as f:
+                json.dump(users, f)
+
+convert_to_json()
+####################################################################
