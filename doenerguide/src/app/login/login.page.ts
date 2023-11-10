28a7d8f43490f327e1b73f1e79f5cea49d4febce
dmncrs <domenic.roos@icloud.com>
2023-11-10 11:25:42 +0100

    Add route

--- doenerguide/src/app/login/login.page.ts
+++ doenerguide/src/app/login/login.page.ts
@@ -2,35 +2,31 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule, RouterModule],
 })
 export class LoginPage implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
-
 
 //add event listener on keypress
 document.addEventListener('keydown', (event) => {
   loginButton(event);
 });
 
-
 function loginButton(event?: KeyboardEvent) {
   var email = document.getElementById('email') as HTMLInputElement;
   var password = document.getElementById('password') as HTMLInputElement;
   var loginbutton = document.getElementById('login') as HTMLButtonElement;
-  if (email.value == "" || password.value == "") {
+  if (email.value == '' || password.value == '') {
     loginbutton.disabled = true;
   } else {
     loginbutton.disabled = false;
@@ -40,27 +36,24 @@ function loginButton(event?: KeyboardEvent) {
   }
 }
 
-
 //function called when login button is clicked
 function login() {
-  console.log("login");
+  console.log('login');
   //get email & password from input fields
   var email = document.getElementById('email') as HTMLInputElement;
   var password = document.getElementById('password') as HTMLInputElement;
-  var email_value = email.value
-  var password_value = password.value
+  var email_value = email.value;
+  var password_value = password.value;
 
   //connection to backend
 
   //if login is successful switch to home page
 }
 
-
 //function called when signup button is clicked
 function signup() {
   //switch to signup page
 }
-
 
 //function calles when continue as guest button is clicked
 function guest() {
