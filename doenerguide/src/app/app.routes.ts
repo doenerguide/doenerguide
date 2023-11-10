22b82bb9ef13d3d8809105749cf1db0ad18794b6
dmncrs <domenic.roos@icloud.com>
2023-11-10 11:18:27 +0100

    Add start page

--- doenerguide/src/app/app.routes.ts
+++ doenerguide/src/app/app.routes.ts
@@ -1,13 +1,20 @@
 import { Routes } from '@angular/router';
+import { NavigationComponent } from './navigation/navigation.component';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
-  },
-  {
     path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
+    component: NavigationComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full',
+      },
+      {
+        path: 'home',
+        loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+      },
+    ],
   },
 ];
--- doenerguide/src/app/home/home.page.ts
+++ doenerguide/src/app/home/home.page.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import { NavigationComponent } from '../navigation/navigation.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, NavigationComponent],
 })
 export class HomePage {
   constructor() {}
