539d500620d527becd98339a9a9145c030359281
DrmedAllel <claudius.caspar.laur@gmail.com>
2024-03-15 12:05:11 +0100

    Banner hinzugefügt

--- doenerguide/src/app/app.routes.ts
+++ doenerguide/src/app/app.routes.ts
@@ -15,14 +15,14 @@ export const routes: Routes = [
         path: 'home',
         loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
       },
+      {
+        path: 'login',
+        loadComponent: () => import('./login/login.page').then(m => m.LoginPage)
+      },
+      {
+        path: 'signup',
+        loadComponent: () => import('./signup/signup.page').then(m => m.SignupPage)
+      },
     ],
-  },
-  {
-    path: 'login',
-    loadComponent: () => import('./login/login.page').then( m => m.LoginPage)
-  },
-  {
-    path: 'signup',
-    loadComponent: () => import('./signup/signup.page').then( m => m.SignupPage)
   },
 ];
