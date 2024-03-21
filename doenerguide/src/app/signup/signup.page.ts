762e2cad31376864c3242c2ddab1f23d4f972bb2
dmncrs <domenic.roos@icloud.com>
2024-03-21 09:29:33 +0100

    Update signup

--- doenerguide/src/app/signup/signup.page.ts
+++ doenerguide/src/app/signup/signup.page.ts
@@ -1,147 +1,111 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-import * as $ from 'jquery';
+import { ToastController } from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
 
 // let endpoint = "https://doenerguide.onrender.com";
-let endpoint = "http://127.0.0.1:5050";
-
+let endpoint = 'http://127.0.0.1:5050';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.page.html',
   styleUrls: ['./signup.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule, ReactiveFormsModule],
 })
+export class SignupPage {
+  @ViewChild('errorMessage', { read: ElementRef }) errorMessage!: ElementRef;
 
-export class SignupPage implements OnInit {
+  constructor(
+    private formBuilder: FormBuilder,
+    private toastCtrl: ToastController,
+    private router: Router
+  ) {}
 
-  constructor() { }
+  form = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(8)]],
+    passwordConfirm: ['', [Validators.required, Validators.minLength(8)]],
+    vorname: ['', Validators.required],
+    nachname: ['', Validators.required],
+    terms: [false, Validators.requiredTrue],
+  });
 
-  //function called when signup button is clicked
-  signup() {
-    let vorname_elem = document.getElementById('vorname') as HTMLInputElement;
-    let nachname_elem = document.getElementById('nachname') as HTMLInputElement;
-    let email_elem = document.getElementById('email') as HTMLInputElement;
-    let password_elem = document.getElementById('password') as HTMLInputElement;
-    let password_confirm_elem = document.getElementById('password_confirm') as HTMLInputElement;
+  checkRegisterButton(): boolean {
+    return !(
+      this.form.get('email')?.valid &&
+      this.form.get('password')?.valid &&
+      this.form.get('passwordConfirm')?.valid &&
+      this.form.get('vorname')?.valid &&
+      this.form.get('nachname')?.valid &&
+      this.form.get('terms')?.valid
+    );
+  }
 
-    let vorname_value = vorname_elem.value;
-    let nachname_value = nachname_elem.value;
-    let email_value = email_elem.value;
-    let password_value = password_elem.value;
-    let password_confirm_value = password_confirm_elem.value;
-
-    if (password_value != password_confirm_value) {
-      let error_message = document.getElementById('error_message');
-      if (error_message) {
-        error_message.innerHTML = 'Passwörter stimmen nicht überein';
-      }
+  register() {
+    this.errorMessage.nativeElement.innerHTML = '';
+    if (this.checkRegisterButton()) {
       return;
     }
 
-    if (password_value.length < 8) {
-      let error_message = document.getElementById('error_message');
-      if (error_message) {
-        error_message.innerHTML = 'Passwort muss mindestens 8 Zeichen lang sein';
-      }
+    if (!this.form.valid) {
+      this.errorMessage.nativeElement.innerHTML =
+        'Bitte füllen alle Felder korrekt aus';
       return;
     }
 
-    if (password_value.length > 20) {
-      let error_message = document.getElementById('error_message');
-      if (error_message) {
-        error_message.innerHTML = 'Passwort darf maximal 20 Zeichen lang sein';
-      }
+    let password = this.form.get('password')?.value;
+    let passwordConfirm = this.form.get('passwordConfirm')?.value;
+
+    if (password !== passwordConfirm) {
+      this.errorMessage.nativeElement.innerHTML =
+        'Passwörter stimmen nicht überein';
       return;
     }
 
-    let regex_mail = new RegExp('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$');
-    if (!regex_mail.test(email_value)) {
-      let error_message = document.getElementById('error_message');
-      if (error_message) {
-        error_message.innerHTML = 'Bitte gib eine gültige E-Mail Adresse ein';
-      }
-      return;
-    }
+    let email = this.form.get('email')?.value;
+    let vorname = this.form.get('vorname')?.value;
+    let nachname = this.form.get('nachname')?.value;
 
-    $.ajax({
-      url: endpoint + '/signup',
-      type: 'POST',
-      data: JSON.stringify({ "email": email_value, "password": password_value, "vorname": vorname_value, "nachname": nachname_value }),
-      contentType: 'application/json; charset=utf-8',
-      dataType: 'json',
+    fetch(endpoint + '/signup', {
+      method: 'POST',
       headers: {
-        "Access-Control-Allow-Origin": "*"
+        'Content-Type': 'application/json; charset=utf-8',
       },
-      success: function (data: any) {
-        let error_message = document.getElementById('error_message');
-        if (data["success"]) {
-          if (error_message) {
-            error_message.innerHTML = 'Erfolgreich registriert';
-          }
-          return;
+      body: JSON.stringify({
+        email: email,
+        password: password,
+        vorname: vorname,
+        nachname: nachname,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.success) {
+          this.toastCtrl
+            .create({
+              message: 'Erfolgreich registriert',
+              duration: 2000,
+              position: 'top',
+              color: 'success',
+              icon: 'checkmark-circle-outline',
+            })
+            .then((toast) => toast.present());
+          setTimeout(() => this.router.navigate(['/login']), 2000);
         } else {
-          if (error_message) {
-            error_message.innerHTML = 'Diese E-Mail Adresse ist bereits registriert';
-          }
-          return;
+          this.errorMessage.nativeElement.innerHTML =
+            'Diese E-Mail Adresse ist bereits registriert';
         }
-      },
-      error: function (xhr: any, status: string, error: any) {
-        console.log(xhr.responseText);
-        console.log(status);
-        console.log(error);
-        return;
-      }
-    });
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
   }
-
-  signupButton() {
-    let vorname = document.getElementById('vorname') as HTMLInputElement;
-    let nachname = document.getElementById('nachname') as HTMLInputElement;
-    let email = document.getElementById('email') as HTMLInputElement;
-    let password = document.getElementById('password') as HTMLInputElement;
-    let password_confirm = document.getElementById('password_confirm') as HTMLInputElement;
-    let signupbutton = document.getElementById('signup') as HTMLButtonElement;
-    let terms = document.getElementById('terms') as HTMLInputElement;
-    if (!terms.checked) {
-      signupbutton.disabled = true;
-      return false;
-    }
-    if (vorname.value == "") {
-      signupbutton.disabled = true;
-      return false;
-    }
-    if (nachname.value == "") {
-      signupbutton.disabled = true;
-      return false;
-    }
-    if (email.value == "") {
-      signupbutton.disabled = true;
-      return false;
-    }
-    if (password.value == "") {
-      signupbutton.disabled = true;
-      return false;
-    }
-    if (password_confirm.value == "") {
-      signupbutton.disabled = true;
-      return false;
-    }
-    signupbutton.disabled = false;
-    return true;
-  }
-
-  ngOnInit() { }
-
 }
-
-document.addEventListener('keyup', (event) => {
-  let isEnabled = SignupPage.prototype.signupButton();
-  if (event.key === 'Enter' && isEnabled) {
-    SignupPage.prototype.signup();
-  }
-});
