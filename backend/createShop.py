9a8f8d3e087e0d6a2825438bce4d89da33ac4d7d
Kagan Demirer <demirerkagan0808@gmail.com>
2024-03-22 11:08:55 +0100

    .

---
+++ backend/createShop.py
@@ -0,0 +1,12 @@
+from databaseManager import create_shop
+
+name = ""
+image_url = ""
+address = ""
+price_category = 0
+opening_hours = ""
+flags = ""
+lat = 0
+long = 0
+
+res = create_shop(name, image_url, address, price_category, opening_hours, flags, lat, long)
