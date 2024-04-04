6025b6857eff458d642f084c34d3f491101aee06
dmncrs <domenic.roos@icloud.com>
2024-04-04 13:36:31 +0200

    Fix shop detail view

--- doenerguide/src/app/shop/shop.page.ts
+++ doenerguide/src/app/shop/shop.page.ts
@@ -5,6 +5,8 @@ import { Shop, ShopFunctions } from '../interfaces/shop';
 import { environment } from 'src/environments/environment';
 import { IonicModule } from '@ionic/angular';
 import { PriceComponent } from '../price/price.component';
+import { DatabaseService } from '../services/database.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-shop',
@@ -14,14 +16,26 @@ import { PriceComponent } from '../price/price.component';
   imports: [IonicModule, CommonModule, FormsModule, PriceComponent],
 })
 export class ShopPage implements OnInit {
-  @Input() id: string = '';
+  //@Input() shop!: Shop;
 
-  shop: Shop | undefined;
+  //shop: Shop | undefined;
+
+  shop: Shop | null = null;
   shopFunctions = ShopFunctions;
 
-  constructor() { }
+  constructor(
+    private databaseSrv: DatabaseService,
+    private route: ActivatedRoute
+  ) {}
 
-  ngOnInit() {
-    this.shop = environment.shops.find((shop: Shop) => shop.id === this.id);
+  /*ngOnInit() {
+    this.databaseSrv.getShop(this.id).then((shop) => (this.shop = shop));
+  }*/
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      console.log(params);
+      this.shop = JSON.parse(params['shop']) as Shop;
+    });
   }
 }
