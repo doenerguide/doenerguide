f1399a99333e63bddce4218bc2b9b665775af6bf
DrmedAllel <claudius.caspar.laur@gmail.com>
2024-04-04 15:11:14 +0200

    Läden added to txt file

--- backend/getLaeden.py
+++ backend/getLaeden.py
@@ -4,6 +4,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 import time
+import logging
+import threading
 
 options = Options()
 options.add_argument("--headless")  # Run Chrome in headless mode
@@ -14,7 +16,7 @@ def writeToFile(file, data):
     with open(file, "w") as f:
         f.write(data)
 
-def getStadt(url):
+def getStadt(url, driver):
     driver.get(url)
     while url == driver.current_url:
         pass
@@ -33,7 +35,6 @@ def getStadt(url):
     scrolled_at_bottom = False
     while not scrolled_at_bottom:
         driver.execute_script("arguments[0].scrollTo(0, arguments[0].scrollHeight)", bar)
-        time.sleep(2)
         try:
             driver.find_element(By.CLASS_NAME, "HlvSq")
             scrolled_at_bottom = True
@@ -63,6 +64,9 @@ if __name__ == "__main__":
         url = "https://www.google.com/maps/search/" + plz + "%20Doenerladen"
         print("URL: " + url)
         print("Already done: " + str(i))
-        print("Remaining: " + str(len(plzs) - i))
         print("Total: " + str(len(plzs)))
-        getStadt(url)
+        getStadt(url, driver)
+        # delete the plz from the list
+        plzs = plzs[1:]
+        with open("backend/plz.txt", "w") as f:
+            f.write("\n".join(plzs))
