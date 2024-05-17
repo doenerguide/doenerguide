77cc99118dae1ad14211d5bffa367caba7db7a6c
dmncrs <domenic.roos@icloud.com>
2024-05-17 10:37:12 +0200

    Fix routes

--- doenerguide/src/app/app.routes.ts
+++ doenerguide/src/app/app.routes.ts
@@ -38,18 +38,18 @@ export const routes: Routes = [
         path: 'account',
         loadComponent: () => import('./account/account.page').then((m) => m.AccountPage),
       },
-      {
-        path: 'shop',
-        loadComponent: () => import('./shop/shop.page').then((m) => m.ShopPage),
-      },
-      {
-        path: 'stempelkarten',
-        loadComponent: () => import('./stempelkarten/stempelkarten.page').then((m) => m.StempelkartenPage),
-      },
-      {
-        path: 'card',
-        loadComponent: () => import('./card/card.page').then((m) => m.CardPage),
-      },
     ],
-  }
+  },
+  {
+    path: 'shop',
+    loadComponent: () => import('./shop/shop.page').then((m) => m.ShopPage),
+  },
+  {
+    path: 'stempelkarten',
+    loadComponent: () => import('./stempelkarten/stempelkarten.page').then((m) => m.StempelkartenPage),
+  },
+  {
+    path: 'card',
+    loadComponent: () => import('./card/card.page').then((m) => m.CardPage),
+  },
 ];
